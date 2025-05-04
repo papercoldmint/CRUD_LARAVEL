261d5a7e2ef6c0441ce86b2ff60d08c42d05d696
papercoldmint <wijayantogangsar@gmail.com>
2025-05-04 23:30:56 +0700

    update index

--- resources/views/makanans/index.blade.php
+++ resources/views/makanans/index.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="d-flex justify-content-between align-items-center mb-4">
-                <h2>Daftar Makanan</h2>
+                <h2>Daftar Makanan foodsar</h2>
                 <a href="{{ route('makanans.create') }}" class="btn btn-primary">Tambah Makanan</a>
             </div>
             
